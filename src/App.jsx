344a764dfefca9623f2aa1eef510dc033379f129
Jair <jairaceves56@gmail.com>
2023-09-19 19:37:47 -0600

    Inyectamos el contenido del componente al Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthLayout } from './layouts/AuthLayout'
+import { Login } from './paginas/Login'
+
 
 function App() {
  
   return (
-    <div className=''>
-      <h1 className="text-emerald-400">Hola</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<Login />} />
+
+        </Route>
+        <Route path='/'>
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
