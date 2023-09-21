3a13be7e39e6027ab4c6e0c23243e0a2bffc4665
Jair <jairaceves56@gmail.com>
2023-09-21 12:56:58 -0600

    Inicializamos la pagina para confirmar cuenta

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom'
+
 
 export const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+      <h1 className="text-center text-sky-600 font-black text-5xl capitalize">Confirma tu cuenta y comienza a crear Tus
+        <span className='text-slate-700'> Proyectos</span> 
+      </h1>
+
+      
+
+    </>
   )
 }
