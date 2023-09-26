1cd5412a25c1206aa3379d12bc0a1735fbf0c054
Jair <jairaceves56@gmail.com>
2023-09-26 13:31:33 -0600

    Agregamos el Provider en la App para usarlo

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,29 @@ import { Registrar } from './paginas/Registrar'
 import { OlvidePassword } from './paginas/OlvidePassword'
 import { NuevoPassword } from './paginas/NuevoPassword'
 import { ConfirmarCuenta } from './paginas/ConfirmarCuenta'
+import { AuthProvider } from './context/AuthProvider'
 
 
 function App() {
  
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPassword />} />
-          <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPassword />} />
+            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
 
-        </Route>
-        <Route path='/'>
+          </Route>
+          <Route path='/'>
 
 
-        </Route>
-      </Routes>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
