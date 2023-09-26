32af7b1c2ff9c8952f5ed22f5e64eccdcec0e7f5
Jair <jairaceves56@gmail.com>
2023-09-26 11:49:52 -0600

    Modificamos el Password

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -30,8 +30,33 @@ export const NuevoPassword = () => {
     comprobarToken()
   }, [])
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
+
+    if(password.length < 6) {
+      setAlerta({
+          msg: 'El Password debe ser minimo de 6 caracteres',
+          error: true
+      })
+      return
+    }
+
+    try {
+      const url = `/usuarios/olvide-password/${token}`
+
+      const { data } = await clienteAxios.post(url, { password })
+      setAlerta({
+          msg: data.msg,
+          error: false
+      })
+      setPasswordModificado(true)
+    } catch (error) {
+        setAlerta({
+            msg: error.response.data.msg,
+            error: true
+        })
+    }
+
   }
 
   const { msg } = alerta
@@ -47,7 +72,7 @@ export const NuevoPassword = () => {
       { tokenValido && (
         <form 
           className='my-10  bg-gray-400 shadow-xl rounded-lg px-10 py-5'
-          onChange={handleSubmit}
+          onSubmit={handleSubmit}
         >
           
           {/* Password */}
@@ -61,6 +86,8 @@ export const NuevoPassword = () => {
               type='password'
               placeholder='Escribe tu Nuevo Password'
               className='w-full mt-1 p-2 border rounded-lg bg-gray-200'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
 
@@ -71,6 +98,13 @@ export const NuevoPassword = () => {
               hover:cursor-pointer hover:bg-sky-900 transition-colors mb-5'
           />
         </form>
+      )}
+
+      {passwordModificado && (
+          <Link 
+              className='block text-center my-5 text-slate-500 uppercase text-sm'
+              to="/"
+          >Inicia Sesión</Link>
       )}
 
     </>
