948f5e868b9060e6e6cf52b7233e681aca0034d2
Jair <jairaceves56@gmail.com>
2023-09-25 19:37:01 -0600

    Validamos el token para validar la cuenta

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -1,14 +1,60 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import clienteAxios from '../config/clienteAxios'
+import { Alerta } from '../components/Alerta'
 
 
 export const ConfirmarCuenta = () => {
+
+  const [alerta, setAlerta] = useState({})
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false)
+
+  const params = useParams();
+  const { id } = params
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+          const url = `/usuarios/confirmar/${id}`
+          const { data } = await clienteAxios(url)
+
+          setAlerta({
+            msg: data.msg,
+            error: false
+          })
+          setCuentaConfirmada(true)
+
+      } catch (error) {
+          setAlerta({
+            msg: error.response.data.msg,
+            error: true
+          })
+      }
+    }
+    confirmarCuenta();
+  }, [])
+
+  const { msg } = alerta
+
+
   return (
     <>
       <h1 className="text-center text-sky-600 font-black text-5xl capitalize">Confirma tu cuenta y comienza a crear Tus
         <span className='text-slate-700'> Proyectos</span> 
       </h1>
 
-      
+      <div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-lg bg-white'>
+        { msg && <Alerta alerta={alerta} /> }
+
+        {cuentaConfirmada && (
+          <div className='flex md:flex-row justify-center my-5'>
+            <Link 
+              className='block text-center md:my-0 my-2 text-slate-500 uppercase text-sm underline'
+              to="/"
+            > Iniciar Sesión</Link>
+          </div>
+        )}
+      </div> 
 
     </>
   )
