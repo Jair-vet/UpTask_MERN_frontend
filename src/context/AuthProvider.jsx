2be16a077fa6dfbd56286c65ceb900e7545faad4
Jair <jairaceves56@gmail.com>
2023-10-18 09:40:09 -0600

    Implementamos el navigate para cuando este autenticado nos direccione siempre a Proyectos

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -29,15 +29,14 @@ const AuthProvider = ({children}) => {
             try {
                 const { data } = await clienteAxios('/usuarios/perfil', config)
                 setAuth(data)
-                // navigate('/proyectos')
+                navigate('/proyectos')
 
             } catch (error) {
                 setAuth({})
 
-            } finally {
-                setCargando(false)
-            }
-
+            } 
+            
+            setCargando(false)
 
             
         }
