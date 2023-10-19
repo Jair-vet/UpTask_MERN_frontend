87d4190a8b07d3929c9b63efa5fad2bfea3ad91d
Jair <jairaceves56@gmail.com>
2023-10-18 19:25:26 -0600

    Creamos la vista basica del SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 export const Sidebar = () => {
 
     return (
-    <aside className="md:w-80 lg:w-96 px-5 py-10">
-        <p className="text-xl font-bold">Hola: <span>Jair</span></p>
+    <aside className="md:w-80 lg:w-96 px-5 py-10 bg-sky-800 shadow-xl">
+        <p className="text-xl font-bold text-white text-center">Bienvenido <span className="text-white">Jair</span></p>
         
         <Link 
             to="/crear-proyecto"
-            className="bg-sky-600 w-full p-3 text-white uppercase font-bold block text-center mt-5 rounded-lg"
+            className="bg-emerald-600 w-full p-3 text-white uppercase font-bold block text-center mt-5 rounded-lg"
         >Nuevo Proyecto
         </Link>
     
