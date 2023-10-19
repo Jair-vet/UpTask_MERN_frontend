d82c53cb5b8b163171699b091ea687dea812c978
Jair <jairaceves56@gmail.com>
2023-10-18 20:20:18 -0600

    Consultamos el Nombre u lo mostramos en pantalla

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
+import useAuth from '../hooks/useAuth'
 
 export const Sidebar = () => {
 
+    const { auth } = useAuth()
+
     return (
     <aside className="md:w-80 lg:w-96 px-5 py-10 bg-sky-800 shadow-xl">
-        <p className="text-xl font-bold text-white text-center">Bienvenido <span className="text-white">Jair</span></p>
+        <p className="text-xl font-light text-white text-center">Bienvenido <span className="text-white font-bold">{ auth.nombre }</span></p>
         
         <Link 
             to="crear-proyecto"
