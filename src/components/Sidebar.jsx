55f3beaa8c77bb55bb19bc53bab1e19d1e1837d5
Jair <jairaceves56@gmail.com>
2023-10-19 18:57:46 -0600

    Agregamos un Icono al boton de Agregar Proyecto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,11 +9,16 @@ export const Sidebar = () => {
     <aside className="md:w-80 lg:w-96 px-5 py-10 bg-sky-800 shadow-xl">
         <p className="text-xl font-light text-white text-center">Bienvenido <span className="text-white font-bold">{ auth.nombre }</span></p>
         
-        <Link 
-            to="crear-proyecto"
-            className="bg-emerald-600 hover:bg-emerald-700 duration-300 w-full p-3 text-white uppercase font-bold block text-center mt-5 rounded-lg"
-        >Nuevo Proyecto
-        </Link>
+        <div className='flex items-center'>
+            <Link 
+                to="crear-proyecto"
+                className="flex items-center justify-center gap-2 bg-emerald-600 hover:bg-emerald-700 duration-300 w-full p-3 text-white uppercase font-bold text-center mt-5 rounded-lg"
+            >Nuevo Proyecto
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+            </Link>
+        </div>
     
     </aside>
 
