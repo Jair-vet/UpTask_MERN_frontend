db98d30c6ea4be62781978c7383b0d1be59013d9
Jair <jairaceves56@gmail.com>
2023-10-19 19:16:56 -0600

    Agregamos la base para poder hacer ediciones en el Proyecto

--- src/paginas/EditarProyecto.jsx
+++ src/paginas/EditarProyecto.jsx
@@ -1,6 +1,29 @@
+import { useParams } from "react-router-dom"
+import useProyectos from "../hooks/useProyectos"
+import { useEffect } from "react"
+import { FormularioProyecto } from "../components/FormularioProyecto"
 
 export const EditarProyecto = () => {
+
+    const params = useParams()
+
+    const { obtenerProyecto, proyecto, cargando } = useProyectos()
+
+    useEffect(() => {
+        obtenerProyecto(params.id)
+    }, [])
+
+    const { nombre } = proyecto
+
+    if(cargando) return 'Cargando...'
+
   return (
-    <div>EditarProyecto</div>
+    <>
+        <h1 className='font-black text-4xl'>Editar Proyecto: { proyecto.nombre }</h1>
+        
+        <div className="mt-10 flex justify-center">
+            <FormularioProyecto />
+        </div>
+    </>
   )
 }
