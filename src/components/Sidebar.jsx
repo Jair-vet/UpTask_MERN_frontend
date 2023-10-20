b4dcb54f3a27468251af0a82bef37f1bfe4fe9b0
Jair <jairaceves56@gmail.com>
2023-10-19 19:06:24 -0600

    Cambiamos el Tamaño del Icono

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ export const Sidebar = () => {
                 to="crear-proyecto"
                 className="flex items-center justify-center gap-2 bg-emerald-600 hover:bg-emerald-700 duration-300 w-full p-3 text-white uppercase font-bold text-center mt-5 rounded-lg"
             >Nuevo Proyecto
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
             </Link>
