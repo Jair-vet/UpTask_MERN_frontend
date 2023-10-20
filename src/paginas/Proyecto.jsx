b879ba22e9c423d327863b0fc32f77d0a73352a8
Jair <jairaceves56@gmail.com>
2023-10-20 12:33:30 -0600

    Agregamos el Componente del Modal para Agregar Tarea

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import useProyectos from '../hooks/useProyectos';
+import ModalFormularioTarea from '../components/ModalFormularioTarea';
 
 export const Proyecto = () => {
 
     const params = useParams()
 
     const { obtenerProyecto, proyecto, cargando } = useProyectos()
+
+    const [modal, setModal] = useState(false)
 
     useEffect(() => {
         obtenerProyecto(params.id)
@@ -22,7 +25,6 @@ export const Proyecto = () => {
         <div className='flex justify-between'>
             <h1 className='font-black text-4xl'>{nombre}</h1>
 
-
             <div className='flex items-center gap-2 text-gray-500 hover:text-emerald-600'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -37,6 +39,7 @@ export const Proyecto = () => {
 
         
         <button
+            onClick={() => setModal(true)}
             type='button'
             className='flex gap-2 item-center justify-center text-md mt-5 px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-600 hover:bg-sky-700 duration-300 text-white text-center'
         >
@@ -45,6 +48,11 @@ export const Proyecto = () => {
             </svg>
             Nueva Tarea
         </button>
+
+        <ModalFormularioTarea 
+            modal={modal}
+            setModal={setModal}
+        />
 
     </>
   )
