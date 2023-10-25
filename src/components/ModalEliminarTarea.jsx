89f4de98612c123a0fd76535f71fd719707f7404
Jair <jairaceves56@gmail.com>
2023-10-25 10:57:46 -0600

    Agregamos el funcionamiento al Boton eliminar

--- src/components/ModalEliminarTarea.jsx
+++ src/components/ModalEliminarTarea.jsx
@@ -5,7 +5,7 @@ import useProyectos from '../hooks/useProyectos'
 
 const ModalEliminarTarea = () => {
  
-    const { modalEliminarTarea, handleModalEliminarTarea } = useProyectos()
+    const { modalEliminarTarea, handleModalEliminarTarea,  eliminarTarea } = useProyectos()
     return (
         <Transition.Root show={ modalEliminarTarea } as={Fragment}>
             <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={ handleModalEliminarTarea }>
@@ -44,7 +44,7 @@ const ModalEliminarTarea = () => {
                             <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                                 <button
                                     type="button"
-                                    className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                    className="bg-white rounded-md text-red-400 hover:text-red-500 focus:outline-none"
                                     onClick={ handleModalEliminarTarea }
                                 >
                                 <span className="sr-only">Cerrar</span>
@@ -56,9 +56,14 @@ const ModalEliminarTarea = () => {
 
 
                             <div className="sm:flex sm:items-start">
+                                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                                    </svg>
+                                </div>
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                                    <Dialog.Title as="h3" className="text-lg leading-6 font-bold text-gray-900">
-                                        
+                                    <Dialog.Title as="h3" className="text-3xl mb-5 text-center text-gray-400 font-extrabold uppercase">
+                                        Eliminar Tarea
                                     </Dialog.Title>
 
                                     <div className='mt-2'>
@@ -68,23 +73,21 @@ const ModalEliminarTarea = () => {
                                     </div>
 
                                     <div>
-                                        <div>
 
                                             <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                                                 <button
                                                     type="button"
                                                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 duration-300 hover:bg-red-700 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                                    // onClick={eliminarTarea}
+                                                    onClick={eliminarTarea}
                                                 >
                                                     Eliminar
                                                 </button>
                                                 <button
                                                     type="button"
-                                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 duration-300 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 sm:mt-0 sm:w-auto sm:text-sm"
-                                                    // onClick={handleModalEliminarTarea}
+                                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 duration-300 hover:text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 sm:mt-0 sm:w-auto sm:text-sm"
+                                                    onClick={ handleModalEliminarTarea }
                                                 > Cancelar</button>
                                             </div>
-                                        </div>
                                     </div>
 
                                 </div>
